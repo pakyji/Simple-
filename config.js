30d2ba52a7a9f84d12d98e1a4ca79696572002a9
pakyji <108614626+pakyji@users.noreply.github.com>
2026-09-22 21:12:50 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    ownerNumber: "393802347902@s.whatsapp.net", // Your official WhatsApp JID
-    botName: "The Syndicate",
-    footer: "Protected by The Syndicate",
-    banner: "https://i.imgur.com/yourbanner.jpg" // Place your custom banner link here
-};
