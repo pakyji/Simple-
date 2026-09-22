651c52ae1e01b01ba7dda03ff1c5eddcc4526436
pakyji <108614626+pakyji@users.noreply.github.com>
2026-09-22 20:54:11 +0200

    Delete Commands directory

--- Commands/menu.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: ',menu',
-    async execute(sock, mek, from) {
-        await sock.sendMessage(from, { text: '🤖 *The Syndicate Menu*\n\n,ping - Test bot speed' }, { quoted: mek });
-    }
-};
--- Commands/owner.js
+++
@@ -1,8 +0,0 @@
-const config = require('../config');
-
-module.exports = {
-    name: ',owner',
-    async execute(sock, mek, from) {
-        await sock.sendMessage(from, { text: `👑 *Owner Information*\n\nThis bot is officially owned and protected by ${config.botName}.` }, { quoted: mek });
-    }
-};
