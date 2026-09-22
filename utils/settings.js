f60679476d22c4a83b5ef06f21f532aa9073f579
pakyji <108614626+pakyji@users.noreply.github.com>
2026-09-23 01:38:53 +0200

    Create settings.js

---
+++ utils/settings.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+
+const settingsPath = path.join(__dirname, "../settings.json");
+
+// Initialize settings file if it doesn't exist
+if (!fs.existsSync(settingsPath)) {
+    fs.writeFileSync(settingsPath, JSON.stringify({}));
+}
+
+function getGroupSettings(groupId) {
+    try {
+        const data = JSON.parse(fs.readFileSync(settingsPath, "utf8"));
+        return data[groupId] || { antiLink: false }; // Default: Anti-link is OFF
+    } catch (error) {
+        return { antiLink: false };
+    }
+}
+
+function setGroupSetting(groupId, key, value) {
+    try {
+        const data = JSON.parse(fs.readFileSync(settingsPath, "utf8"));
+        if (!data[groupId]) {
+            data[groupId] = { antiLink: false };
+        }
+        data[groupId][key] = value;
+        fs.writeFileSync(settingsPath, JSON.stringify(data, null, 2));
+    } catch (error) {
+        console.error("Error saving group setting:", error);
+    }
+}
+
+module.exports = { getGroupSettings, setGroupSetting };
