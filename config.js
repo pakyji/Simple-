68ca0f4807fe9d1298a88dfd0881cb8904333fca
pakyji <108614626+pakyji@users.noreply.github.com>
2026-09-22 18:23:13 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ownerNumber: "393802347902@s.whatsapp.net", // Your official WhatsApp JID
+    botName: "The Syndicate",
+    footer: "Protected by The Syndicate",
+    banner: "https://i.imgur.com/yourbanner.jpg" // Place your custom banner link here
+};
