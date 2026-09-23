63475825cfa792d644e0f8fb3f815decb69d3349
pakyji <108614626+pakyji@users.noreply.github.com>
2026-09-23 10:41:55 +0200

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -3,7 +3,6 @@ module.exports = {
     description: "Shows the bot community link",
     execute: async (sock, msg, sender) => {
         const ownerText = 
-            `Bot Developer / join us\n` +
             `Discord Server: https://discord.gg/syndicateps`;
 
         await sock.sendMessage(sender, { text: ownerText }, { quoted: msg });
